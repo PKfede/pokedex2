56aabde2f4a4a6c5253c4acd845c02464053ef39
Fede <47314404+PKfede@users.noreply.github.com>
2024-07-12 12:37:52 -0600

    Modified pagination

--- src/views/Pokedex.tsx
+++ src/views/Pokedex.tsx
@@ -6,61 +6,61 @@ import CardInfo from "../components/CardInfo";
 import Pagination from "../components/Pagination";
 
 const Pokedex = () => {
-  const [list, setList] = useState([]);
-  const [pokemon, setPokemon] = useState({});
-  const [search, setSearch] = useState("mew");
-  const [page, setPage] = useState(0);
-  const [count, setCount] = useState(0);
+    const [list, setList] = useState([]);
+    const [pokemon, setPokemon] = useState({});
+    const [search, setSearch] = useState("pikachu");
+    const [page, setPage] = useState(1);
+    const [count, setCount] = useState(0);
 
-  const fetchAllPokemon = async () => {
-    try {
-      const fetchData = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=5&offset=${page}`
-      );
-      const data = await fetchData.json();
-      setList(data.results);
-      setCount(data.count);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const fetchAllPokemon = async () => {
+        try {
+            const fetchData = await fetch(
+                `https://pokeapi.co/api/v2/pokemon?limit=5&offset=${(page - 1) * 5}`
+            );
+            const data = await fetchData.json();
+            setList(data.results);
+            setCount(data.count);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  const fetchOne = async () => {
-    try {
-      const fetchData = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${search} `
-      );
-      const data = await fetchData.json();
-      setPokemon({ ...data });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const fetchOne = async () => {
+        try {
+            const fetchData = await fetch(
+                `https://pokeapi.co/api/v2/pokemon/${search} `
+            );
+            const data = await fetchData.json();
+            setPokemon({ ...data });
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  useEffect(() => {
-    fetchAllPokemon();
-  }, [, page]);
+    useEffect(() => {
+        fetchAllPokemon();
+    }, [, page]);
 
-  useEffect(() => {
-    fetchOne();
-  }, [search]);
+    useEffect(() => {
+        fetchOne();
+    }, [search]);
 
-  return (
-    <>
-      <div className={style.layout}>
-        <div className={style.list}>
-          {list.map((pokemon, index) => {
-            return <Card key={index} data={pokemon} setSearch={setSearch} />;
-          })}
-          {count && <Pagination count={count} page={page} setPage={setPage} />}
-        </div>
-        <div className={style.info}>
-          <Search setSearch={setSearch} setCount={setCount} />
-          <CardInfo pokemon={pokemon} />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={style.layout}>
+                <div className={style.list}>
+                    {list.map((pokemon, index) => {
+                        return <Card key={index} data={pokemon} setSearch={setSearch} />;
+                    })}
+                    {count && <Pagination count={count} page={page} setPage={setPage} />}
+                </div>
+                <div className={style.info}>
+                    <Search setSearch={setSearch} setCount={setCount} />
+                    <CardInfo pokemon={pokemon} />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Pokedex;
