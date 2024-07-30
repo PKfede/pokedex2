9afdadb0f65c00ff8703bb13b9ebba7dce4fe4c7
PKfede <47314404+PKfede@users.noreply.github.com>
2024-07-30 00:03:01 -0600

    added more displayed pokemon information

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -1,24 +1,57 @@
-
+import typeLibrary from "../assets/typesImg/assetIndex";
+import styles from "../styles/cardInfo.module.css";
 
 const CardInfo = ({ pokemon }: any) => {
+  return (
+    <>
+      <div className={styles.InfoLayout}>
+        <h1>{pokemon.name}</h1>
 
-    return (
-        <>
-            <div>
-                <h1>{pokemon.name}</h1>
+        {pokemon.sprites && (
+          <img
+            className={styles.PokeImg}
+            src={pokemon.sprites.other["official-artwork"].front_default}
+            alt=""
+          />
+        )}
 
-                <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
-                {
-                    pokemon.types &&
-                    <div>
-                        {pokemon.types.map((val: any, index: number) => {
-                            return <p key={index}>{val.type.name}</p>
-                        })}
-                    </div>
-                }
-            </div>
-        </>
-    )
-}
+        {pokemon.types && (
+          <div>
+            {pokemon.types.map((val: any, index: number) => {
+              return (
+                <img
+                  key={index}
+                  className={styles.typeImg}
+                  src={typeLibrary[val.type.name as keyof typeof typeLibrary]}
+                  alt=""
+                />
+              );
+            })}
+          </div>
+        )}
+        <div className={styles.AbilitiesLayout}>
+          <h1>Abilities</h1>
+          <>
+            {pokemon.abilities &&
+              pokemon.abilities.map((val: any, index: number) => {
+                return (
+                  <p key={index} className={styles.Abilities}>
+                    {" "}
+                    {val.ability.name}
+                  </p>
+                );
+              })}
+          </>
+        </div>
 
-export default CardInfo
+        <div className={styles.Stats}>
+          <p>Height: {pokemon.height}</p>
+          <p>Weight: {pokemon.weight}</p>
+          <p>Base experience: {pokemon.base_experience}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardInfo;
--- src/views/Pokedex.tsx
+++ src/views/Pokedex.tsx
@@ -6,78 +6,76 @@ import CardInfo from "../components/CardInfo";
 import Pagination from "../components/Pagination";
 
 interface PokeName {
-    name: string,
-    url: string,
-
+  name: string;
+  url: string;
 }
 
 const Pokedex = () => {
-    const [list, setList] = useState([]);
-    const [pokemon, setPokemon] = useState({});
-    const [search, setSearch] = useState("");
-    const [page, setPage] = useState(1);
-    const [count, setCount] = useState(0);
+  const [list, setList] = useState([]);
+  const [pokemon, setPokemon] = useState({});
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
 
-    const fetchAllPokemon = async () => {
-        try {
-            const fetchData = await fetch(
-                `https://pokeapi.co/api/v2/pokemon?limit=5&offset=${(page - 1) * 5}`
+  const fetchAllPokemon = async () => {
+    try {
+      const fetchData = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=5&offset=${(page - 1) * 5}`
+      );
+      const data = await fetchData.json();
+      setList(data.results);
+      setCount(data.count);
+      setSearch(data.results[0].name);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchOne = async () => {
+    try {
+      const fetchData = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${search} `
+      );
+      const data = await fetchData.json();
+      setPokemon(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllPokemon();
+  }, [page]);
+
+  useEffect(() => {
+    fetchOne();
+  }, [search]);
+
+  return (
+    <>
+      <div className={style.layout}>
+        <div className={style.list}>
+          {list.map((poke: PokeName, index) => {
+            const id = poke.url.split("/");
+
+            return (
+              <Card
+                key={index}
+                id={id[id.length - 2]}
+                data={poke}
+                setSearch={setSearch}
+              />
             );
-            const data = await fetchData.json();
-            setList(data.results);
-            setCount(data.count);
-            setSearch(data.results[0].name)
-
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const fetchOne = async () => {
-        try {
-            const fetchData = await fetch(
-                `https://pokeapi.co/api/v2/pokemon/${search} `
-            );
-            const data = await fetchData.json();
-            setPokemon(data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchAllPokemon();
-    }, [page]);
-
-    useEffect(() => {
-        fetchOne();
-    }, [search]);
-
-    console.log(search, list);
-
-
-
-    return (
-        <>
-            <div className={style.layout}>
-                <div className={style.list}>
-                    {list.map((poke: PokeName, index) => {
-                        const id = poke.url.split("/")
-
-                        return <Card key={index} id={id[id.length - 2]} data={poke} setSearch={setSearch} />;
-                    })}
-                    {count && <Pagination count={count} page={page} setPage={setPage} />}
-                </div>
-                <div className={style.info}>
-                    <Search setSearch={setSearch} setCount={setCount} />
-                    {
-                        !pokemon && <CardInfo pokemon={pokemon} />
-                    }
-
-                </div>
-            </div>
-        </>
-    );
+          })}
+          {count && <Pagination count={count} page={page} setPage={setPage} />}
+        </div>
+        <div className={style.info}>
+          <Search setSearch={setSearch} setCount={setCount} />
+          <CardInfo pokemon={pokemon} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Pokedex;
